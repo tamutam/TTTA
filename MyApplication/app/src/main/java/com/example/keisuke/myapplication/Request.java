e864a00db967e4556504262e31ec079153dbbd9d
keisuke-homePC <tabletennis_tamu@yahoo,co.jp>
2015-03-22 02:49:15 +0900

    valley導入できたのかな

---
+++ MyApplication/app/src/main/java/com/example/keisuke/myapplication/Request.java
@@ -0,0 +1,39 @@
+package com.example.keisuke.myapplication;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+/**
+ * Created by keisuke on 2015/03/22.
+ */
+public class Request {
+   // java String tagJsonObj = “json_obj_req;
+   // mRequestQueue = Volley.newRequestQueue(context);
+
+    public static JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+            // HTTPメソッド名を設定する。GETかPOSTか等
+            Request.Method.GET
+            // リクエスト先のURLを設定する
+            ,"http://api.dribbble.com/shots/everyone"
+            // リクエストパラメーターを設定する
+            ,null
+            // 通信成功時のリスナーを設定する
+            ,new Response.Listener() {
+        @Override
+        public void onResponse(JSONObject response) {
+            // 通信成功時の処理
+        }
+    }
+            // 通信失敗時のリスナーを設定する
+            ,new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            // 通信失敗時の処理
+        }
+    }
+    );
+
+  //  mRequestQueue.add(jsonObjReq, tagJsonObj);
+}
